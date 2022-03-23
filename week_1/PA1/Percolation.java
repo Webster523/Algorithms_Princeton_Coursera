51aa5c5a8e4ba4948990a433434172af4d9eb615
Webster <765121851@qq.com>
2022-03-23 20:41:07 +0800

    update Percolation.java to overcome backwash and include a given testing file

--- week_1/PA1/Percolation.java
+++ week_1/PA1/Percolation.java
@@ -5,10 +5,11 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private final int size;
-    private int cntOpen;
-    private boolean[] open;
-    private final WeightedQuickUnionUF uf;
+    private final WeightedQuickUnionUF uf;  // maintain the connectivity
+    private final WeightedQuickUnionUF ufWithoutBottom; // overcome the backwash
+    private final int size;     // the dimension of the grid
+    private int cntOpen;        // the number of open sites
+    private boolean[] open;     // maintain if a site is open or blocked
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
@@ -32,16 +33,13 @@ public class Percolation {
             }
         }
         uf = new WeightedQuickUnionUF(n * n + 2);
-    }
-
-    private int indexMapping(int row, int col) {
-        return (row - 1) * size + col;
+        ufWithoutBottom = new WeightedQuickUnionUF(n * n + 1);
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
         // for corner case - row or col not in range [1, n]
-        if (row < 1 || row > size || col < 1 || col > size) {
+        if (!isvalid(row) || !isvalid(col)) {
             throw new IllegalArgumentException();
         }
 
@@ -58,6 +56,7 @@ public class Percolation {
                 left = indexMapping(row, col - 1);  // stick to the modular programming!
                 if (open[left]) {
                     uf.union(index, left);
+                    ufWithoutBottom.union(index, left);
                 }
             }
 
@@ -66,6 +65,7 @@ public class Percolation {
                 right = indexMapping(row, col + 1);
                 if (open[right]) {
                     uf.union(index, right);
+                    ufWithoutBottom.union(index, right);
                 }
             }
 
@@ -78,6 +78,7 @@ public class Percolation {
             }
             if (open[up]) {
                 uf.union(index, up);
+                ufWithoutBottom.union(index, up);
             }
 
 
@@ -90,14 +91,29 @@ public class Percolation {
             }
             if (open[down]) {
                 uf.union(index, down);
+                if (down != open.length - 1)
+                    ufWithoutBottom.union(index, down);
             }
         }
+    }
+
+    // helper method 1: check whether the arg coordinate is valid or not
+    private boolean isvalid(int num) {
+        if (1 <= num && num <= size)
+            return true;
+        else
+            return false;
+    }
+
+    // helper method 2: map 2D coordinate into 1D index
+    private int indexMapping(int row, int col) {
+        return (row - 1) * size + col;
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
         // for corner case - row or col not in range [1, n]
-        if (row < 1 || row > size || col < 1 || col > size) {
+        if (!isvalid(row) || !isvalid(col)) {
             throw new IllegalArgumentException();
         }
 
@@ -108,19 +124,19 @@ public class Percolation {
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         // for corner case - row or col not in range [1, n]
-        if (row < 1 || row > size || col < 1 || col > size) {
+        if (!isvalid(row) || !isvalid(col)) {
             throw new IllegalArgumentException();
         }
 
         int index = indexMapping(row, col);
-        return uf.find(0) == uf.find(index);
+        return ufWithoutBottom.find(0) == ufWithoutBottom.find(index);
         // return uf.connected(0, index);
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
         return cntOpen;
-    // The spec requires that all instance methods constant time, so just maintain a cntOpen variable.
+    // The spec requires that all instance methods take constant time, so just maintain a cntOpen variable.
     //     int count = 0;
     //     for (int i = 1; i < open.length - 1; i++) {
     //         if (open[i])
